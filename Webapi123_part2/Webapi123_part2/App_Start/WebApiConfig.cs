60f2095c6da1f61817413e2137a9d84afa1316a2
gokulyc <gokulyc@gmail.com>
2019-06-08 23:26:47 +0530

    Webconfig modified
    cors added, bootstap updated, Employee DB EF, JSON formatter

--- Webapi123_part2/Webapi123_part2/App_Start/WebApiConfig.cs
+++ Webapi123_part2/Webapi123_part2/App_Start/WebApiConfig.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
@@ -25,6 +27,11 @@ namespace Webapi123_part2
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+            config.Formatters.Clear();
+            GlobalConfiguration.Configuration.Formatters.Add(new JsonMediaTypeFormatter());
+
         }
     }
 }
---
+++ Webapi123_part2/Webapi123_part2/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Webapi123_part2.Models;
+
+namespace Webapi123_part2.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return Ok(entities.Employees.ToList());
+            }
+        }
+    }
+}
